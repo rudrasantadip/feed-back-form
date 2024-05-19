2fc80cccb03f7371f7485666336267f91d606c39
rudrasantadip <santadip9@gmail.com>
2024-05-19 17:57:35 +0530

    Added a lot of things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalComponent } from './components/personal/personal.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'personal',component:PersonalComponent},
+  {path:'verify',component:VerifyComponent},
+  {path:'feedback',component:FeedbackComponent},
+
+  {path:'',redirectTo:'personal',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { PersonalComponent } from './components/personal/personal.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    PersonalComponent,
+    VerifyComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
